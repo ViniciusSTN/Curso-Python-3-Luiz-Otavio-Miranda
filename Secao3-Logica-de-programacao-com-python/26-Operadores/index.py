204412b8cb0e7cbeff3145e4d802d6e30a38d3d9
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-14 20:36:51 -0300

    aula26

--- Secao3-Logica-de-programacao-com-python/26-Operadores/index.py
+++ Secao3-Logica-de-programacao-com-python/26-Operadores/index.py
@@ -7,7 +7,7 @@ print('Subtração', subtracao)
 multiplicacao = 10 * 10
 print('Multiplicação', multiplicacao)
 
-divisao = 10 / 2.2
+divisao = 10 / 2.2 # sempre float
 print('Divisão', divisao)
 
 divisao_inteira = 10 // 2.2
@@ -15,3 +15,6 @@ print('Divisão Inteira', divisao_inteira) # retorna float
 
 exponenciacao = 2 ** 10
 print('Exponenciação', exponenciacao)
+
+modulo = 25 % 2
+print('Módulo', modulo)
