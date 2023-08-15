b6c3e7697277ada7631021b00745e961b6d549f3
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-15 17:32:58 -0300

    aula37

---
+++ Secao3-Logica-de-programacao-com-python/37-Operador-relacional/index.py
@@ -0,0 +1,14 @@
+# mema coisa do JS
+# pelo jeito só não existe comparador de tipos (=== e !==)
+
+# shell interative
+# python -i <nomeDoArquivo.py>
+
+numero1 = 'A'
+numero2 = 'B'
+
+# nesse caso compara usando a tabela unicode
+if numero1 > numero2:
+  print('n1 é maior')
+else:
+  print('n2 é maior')
