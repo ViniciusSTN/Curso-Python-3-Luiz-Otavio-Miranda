55ae647893f47f44e030b1cc11446ecd6609d22f
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-15 17:02:02 -0300

    aula36

---
+++ Secao3-Logica-de-programacao-com-python/36-Debugger-do-vs_e_interpretador-python/index.py
@@ -0,0 +1,19 @@
+# if / elif / else
+# identar
+
+teste = True;
+
+if teste:
+  print('Oi')
+else:
+  print('olá')
+
+print('Fora dos blocos')
+
+teste2 = False
+teste3 = False
+teste4 = False
+teste5 = False
+teste6 = False
+teste7 = False
+teste8 = False
