688b086cbd0d610a6d62ee19dbacd1ab153b7587
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-15 21:42:01 -0300

    aula43

--- Secao3-Logica-de-programacao-com-python/40-Operador-logico-and/index.py
+++ Secao3-Logica-de-programacao-com-python/40-Operador-logico-and/index.py
@@ -3,6 +3,7 @@
 # falsy -> 0 0.0 '' False (tem outros, mais pra frente)
 # None
 
+# avaliação de curo circuito
 print(True and False and True);
 print(True and 0 and True);
 
---
+++ Secao3-Logica-de-programacao-com-python/41-Operador-logico-or/index.py
@@ -0,0 +1,5 @@
+# or
+
+# avaliação de curo circuito
+# retorna primeiro true
+print(1 or False or 0 or 'abc')
