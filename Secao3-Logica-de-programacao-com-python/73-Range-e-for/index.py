311dcf681de17ddbddf53fc3985025566436e575
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-18 15:23:12 -0300

    aula73

---
+++ Secao3-Logica-de-programacao-com-python/73-Range-e-for/index.py
@@ -0,0 +1,13 @@
+# for + range
+
+# range -> range(start, stop, step)
+
+numeros = range(10)
+numeros2 = range(5, 10)
+numeros3 = range(2, 10, 2)
+numeros4 = range(-2, -10, -2)
+# print(numeros)
+# print(numeros[4])
+
+for valor in numeros4:
+  print(valor)
