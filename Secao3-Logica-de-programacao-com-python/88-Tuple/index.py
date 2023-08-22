c1096dd81b1904219fddb2cb6a98c0856edb3069
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-21 22:09:38 -0300

    aula88

---
+++ Secao3-Logica-de-programacao-com-python/88-Tuple/index.py
@@ -0,0 +1,13 @@
+# Tuple - uma lista imutável
+
+# A tuple é mais eficiente, e quando quiser uma lista que não for alterável, criar uma tuple
+
+# exemplo de lista:
+# nomes = ['Maria', 'Helena', 'Luiz', 'João']
+# nomes[1] = 'outro'
+
+
+# para criar uma tuple, basta tirar os [] ou colocar ()
+nomes = 'Maria', 'Helena', 'Luiz', 'João'
+# nomes[1] = 'outro'   # gera erro
+print(nomes)
