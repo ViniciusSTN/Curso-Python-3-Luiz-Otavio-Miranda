0d99cc12e541a92dbf3e87176041b116d4930cad
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-31 17:55:01 -0300

    aula157

---
+++ Secao4-Python-Funcoes-Dicionarios-Modulos-e-mais/157-Introducao-a-packages/index.py
@@ -0,0 +1,10 @@
+from sys import path
+
+# import package_aula.modulo
+# from package_aula.modulo import variavel
+# from package_aula.modulo import *
+# from package_aula import modulo
+
+# print(*path, sep='\n')
+
+# print(variavel)
