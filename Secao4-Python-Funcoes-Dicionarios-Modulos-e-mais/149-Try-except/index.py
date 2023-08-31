5dc5eb40a782a3837688fb60e6dbdd42a690ea53
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-31 16:07:00 -0300

    aula150

---
+++ Secao4-Python-Funcoes-Dicionarios-Modulos-e-mais/149-Try-except/index.py
@@ -0,0 +1,25 @@
+# (Parte1) try e except para tratar exceções
+
+# a = 18
+# b = 0
+# c = a / b
+
+try:
+    a = 18
+    b = 0
+    # print(b[0])
+    print('Linha 1'[1000])
+    c = a / b
+    print('Linha 2')
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+except NameError:
+    print('Nome b não está definido')
+except (TypeError, IndexError): # tratar dois erros em uma linha
+    print('TypeError + IndexError')
+except Exception:
+    print('ERRO DESCONHECIDO.')
+
+print('CONTINUAR')
+
+# ZeroDivisionError, NameError são exemplos de classes que extendem Exception
